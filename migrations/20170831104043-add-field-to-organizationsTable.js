bf9fd122997ed137141a320acf03887793662a90
suman <suman@wrctechnologies.com>
2017-08-31 16:14:35 +0530

    update by sujit

---
+++ migrations/20170831104043-add-field-to-organizationsTable.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return [queryInterface.addColumn(
+      'jobcategories',
+      'short_desc',
+      {
+        type: Sequelize.STRING(150),after: 'image'
+      }
+      ),
+    queryInterface.addColumn(
+      'jobcategories',
+      'background_image',
+      {
+      type: Sequelize.STRING(150),
+      after: 'short_desc'
+    }
+    )
+    ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
