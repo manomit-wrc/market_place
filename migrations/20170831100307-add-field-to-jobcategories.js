789fa3211ba148d84999c7f73b0501d9def0ce5e
tanaycemk <tanay.cemk@gmail.com>
2017-08-31 15:37:42 +0530

    upload

---
+++ migrations/20170831100307-add-field-to-jobcategories.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+      return [
+         queryInterface.addColumn(
+           'jobcategories',
+           'short_desc',
+           {
+             type: Sequelize.STRING(150),
+             after: 'image'
+             
+           }
+         )
+       ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
