ba81331ab0baa4f5612454a0db62476c641170c2
tanaycemk <tanay.cemk@gmail.com>
2017-09-12 11:05:56 +0530

    upload by sobhan

--- public/frontend/angular/MainCtrl.js
+++ public/frontend/angular/MainCtrl.js
@@ -186,4 +186,28 @@ MainCtrl.controller('MainController', function ($scope, $http, $sce, $routeParam
         restrict: "A",
         link: linker
     }
+}).directive('ngMatch', function($parse){
+  return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            if (!ctrl) return;
+            if (!attrs['ngMatch']) return;
+
+            var firstPassword = $parse(attrs['ngMatch']);
+
+            var validator = function (value) {
+              var temp = firstPassword(scope),
+              v = value === temp;
+              ctrl.$setValidity('match', v);
+              return value;
+          }
+
+          ctrl.$parsers.unshift(validator);
+          ctrl.$formatters.push(validator);
+          attrs.$observe('ngMatch', function () {
+          validator(ctrl.$viewValue);
+          });
+        }
+    };
 });
